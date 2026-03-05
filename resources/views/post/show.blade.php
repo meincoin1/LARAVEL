8f9aa77f74d7c8aebbf3216cc335874d96972ff8
Midina <milina_1979@list.ru>
2026-03-04 23:06:13 -0800

    Директива @foreach

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -4,10 +4,19 @@
     <title>title</title>
 </head>
 <body>
-    @if(count($arr) >= 1)
-        в массиве есть записи
-    @else
-        в массиве нет записей
-    @endif
+    <!-- Перебераем массив циклом. Работает -->
+    <!-- @foreach ($arr as $elem)
+        {{$elem}}
+    @endforeach -->
+
+    <!-- Выводим каждый элемент массива в отдельном абзаце. Работает -->
+    <!-- @foreach ($arr as $elem)
+        <p>{{$elem}}</p>
+    @endforeach -->
+
+    <!-- Выведем элементы массива в виде списка. Работает-->
+     @foreach ($arr as $elem)
+        <li>{{$elem}}</li>
+    @endforeach
 </body>
 </html>
