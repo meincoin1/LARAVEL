ee52968d61969bb1b3226cf62d69af7704a15a06
Midina <milina_1979@list.ru>
2026-03-17 19:11:41 -0700

    Блок PHP

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 	
 class PostController extends Controller
-	{
-		public function show()
-		{
-			return view('post.show', [
-				'arr'=>[1, 2, 5, 6, 3, 7, 8, 10, 11, 12]
-			]);
-		}
-	}
+// 	{
+// 		public function show()
+// 		{
+// 			return view('post.show', [
+// 			]);
+// 		}
+// 	}
 
-?>
+// ?>
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -4,8 +4,8 @@
     <title>title</title>
 </head>
 <body>
-    @for ($i = 0; $i < 10; $i++)
-        значение счетчика: {{ $i }}
-    @endfor
+    @php
+        echo 123;
+    @endphp
 </body>
 </html>
