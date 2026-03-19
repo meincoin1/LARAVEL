ca4dbbd7e72e6897e58b39338166ede4e7f7220d
Midina <milina_1979@list.ru>
2026-03-18 23:42:26 -0700

    Задание 27

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,28 +2,12 @@
 <html>
 <head>
     <title>title</title>
-    <style>
-        table {
-            width: 10%;
-            margin: 20px 0;
-        }
-
-        th, td {
-            border: 1px solid black;
-            padding: 10px;
-            text-align: center;
-        }
-    </style>
 </head>
 <body>
-    <table>
-        @foreach ($matrix as $row)
-            <tr>
-                @foreach ($row as $cell)
-                    <td>{{  $cell }}</td>
-                @endforeach
-            </tr>
+    <ul>
+        @foreach ($employees as $emplo)
+            <li>Имя: {{$emplo['name'] }}. Фамилия: {{ $emplo['surname'] }}. Зарплата: {{ $emplo['salary'] }}</li>
         @endforeach
-    </table>
+    </ul>
 </body>
 </html>
