822949a81c1faf2a318569c6a22f83fafdfce940
Midina <milina_1979@list.ru>
2026-03-19 00:31:16 -0700

    Задание 32

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,12 +2,26 @@
 <html>
 <head>
     <title>title</title>
+    <style>
+        .first {
+            color: red;
+            font-weight: 800;
+        }
+        .last {
+            color: green;
+            font-size: 20px;
+        }
+    </style>
 </head>
 <body>
     <ul>
         @foreach ($string as $str)
-            
-            <li> {{ $loop->iteration}} {{ $str }}</li>
+            @php
+            $class = '';
+            if($loop->first) $class = 'first';
+            if($loop->last) $class = 'last';
+            @endphp
+            <li class = "{{ $class }}">{{ $str }}</li>
         @endforeach
     </ul>
 </body>
