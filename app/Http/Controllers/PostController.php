7368ab771ecd3196a580c419e2f03cc6c7c6cd50
Midina <milina_1979@list.ru>
2026-03-19 00:08:49 -0700

    Задание 29

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,26 +6,10 @@ class PostController extends Controller
 	{
 		public function show()
 		{
-			$employees = [
-		[
-			'name' => 'user1',
-			'surname' => 'surname1',
-			'salary' => 1000,
-		],
-		[
-			'name' => 'user2',
-			'surname' => 'surname2',
-			'salary' => 2000,
-		],
-		[
-			'name' => 'user3',
-			'surname' => 'surname3',
-			'salary' => 3000,
-		],
-	];
-
+			// $users = ['Вика', 'Оля', 'Наташа'];
+			$users = [];
 			return view('post.show', [
-				'employees'=>$employees
+				'users'=>$users
 			]);
 		}
 	}
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,28 +2,12 @@
 <html>
 <head>
     <title>title</title>
-    <style>
-        table {
-            width: 10%;
-            margin: 20px 0;
-        }
-
-        th, td {
-            border: 1px solid black;
-            padding: 10px;
-            text-align: center;
-        }
-    </style>
 </head>
 <body>
-    <table>
-        @foreach ($employees as $emplo)
-           <tr>
-            @foreach ($emplo as $yees)
-                 <td>{{  $yees }}</td>
-             @endforeach
-            </tr>
-        @endforeach
-    </table>
+    @forelse($users as $user)
+        <p>{{ $user }}</p>
+    @empty
+        <p>В массиве нет элементов</p>
+    @endforelse
 </body>
 </html>
