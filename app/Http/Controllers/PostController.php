9afdb07f554becc1dcd38a062edadd9f56d66ad0
Midina <milina_1979@list.ru>
2026-03-25 00:17:40 -0700

    Метод ALL

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers;
 	
-class PostController extends Controller
-	{
-		public function show()
-		{
-			$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-			return view('post.show', [
-				'numbers'=>$numbers
-			]);
-		}
-	}
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Str;
+
+Collection::macro('toUpper', function () {
+    return $this->map(function ($value) {
+        return Str::upper($value);
+    });
+});
+
+$collection = collect([1, 2, 3])->all();
+
+dd($collection)
 
 ?>
