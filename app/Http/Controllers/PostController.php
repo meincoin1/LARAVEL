1084d72b145f994fea66c1ea24f48b2eddd76605
Midina <milina_1979@list.ru>
2026-03-26 02:19:47 -0700

    Метод has

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,17 +12,10 @@ Collection::macro('toUpper', function () {
     });
 });
 
-$data = new Collection([
-    10 => ['user' => 1, 'skill' => 1, 'roles' => ['Role_1', 'Role_3']],
-    20 => ['user' => 2, 'skill' => 1, 'roles' => ['Role_1', 'Role_2']],
-    30 => ['user' => 3, 'skill' => 2, 'roles' => ['Role_1']],
-    40 => ['user' => 4, 'skill' => 2, 'roles' => ['Role_2']],
-]);
+$collection = collect(['account_id' => 1, 'product' => 'Desk', 'amount' => 5]);
 
-$result = $data->groupBy(['skill', function ($item) {
-    return $item['roles'];
-}], preserveKeys: true);
+$collection->has(['amount', 'price']);
 
-dd($result)
+dd($collection)
 
 ?>
