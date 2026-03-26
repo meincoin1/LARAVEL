0874d35cd1f576f341855b49bb06670ad12263e5
Midina <milina_1979@list.ru>
2026-03-26 02:07:13 -0700

    Метод firstWhere

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,9 +12,15 @@ Collection::macro('toUpper', function () {
     });
 });
 
-collect([1, 2, 3, 4])->first(function ($value, $key) {
-    return $value > 2;
-});
+$collection = collect([
+    ['name' => 'Regena', 'age' => null],
+    ['name' => 'Linda', 'age' => 14],
+    ['name' => 'Diego', 'age' => 23],
+    ['name' => 'Linda', 'age' => 84],
+]);
 
+$collection->firstWhere('name', 'Linda');
+
+dd($collection)
 
 ?>
