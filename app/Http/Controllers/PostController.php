615f8876d78f6e50afcb343fdaccaa17a34dddd1
Midina <milina_1979@list.ru>
2026-03-31 22:06:25 -0700

    Метод sliding

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,13 +15,13 @@ class PostController extends Controller
             });
         });
 
-$collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+$collection = collect([1, 2, 3, 4, 5]);
 
-$slice = $collection->slice(4);
+$chunks = $collection->sliding(2);
 
-$slice->all();
+$chunks->toArray();
 
-dd($slice);
+dd($chunks);
     }
 }
 
