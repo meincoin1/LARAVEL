21c095fea855aeec7a41f85c343ca473754a8bfb
Midina <milina_1979@list.ru>
2026-04-09 00:13:19 -0700

    Метод whereIn

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,13 +17,12 @@ class PostController extends Controller
 
 $collection = collect([
     ['product' => 'Desk', 'price' => 200],
-    ['product' => 'Chair', 'price' => 80],
+    ['product' => 'Chair', 'price' => 100],
     ['product' => 'Bookcase', 'price' => 150],
-    ['product' => 'Pencil', 'price' => 30],
     ['product' => 'Door', 'price' => 100],
 ]);
 
-$filtered = $collection->whereBetween('price', [100, 200]);
+$filtered = $collection->whereIn('price', [150, 200]);
 
 $filtered->all();
 
