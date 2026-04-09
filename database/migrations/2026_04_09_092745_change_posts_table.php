bc943d6ff0c067aa70dece90465b42b82c191b33
Midina <milina_1979@list.ru>
2026-04-09 02:31:36 -0700

    Удаление полей в миграциях в Laravel

--- database/migrations/2026_04_09_092745_change_posts_table.php
+++ database/migrations/2026_04_09_092745_change_posts_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-
 class ChangePostsTable extends Migration
 {
-    public function up()
-	{
-		Schema::table('startonemigrate', function (Blueprint $table) {
-			$table->string('title', 100)->change();
-			$table->text('desc');
-		});
-	}
-}
 
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn(['name', 'desc']);
+	});
+    }
+
+};
