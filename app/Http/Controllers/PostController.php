5990c326ca0748a3cca33031c4a3f675e7ed8167
Midina <milina_1979@list.ru>
2026-04-09 00:12:53 -0700

    Метод whereBetween

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,12 +17,13 @@ class PostController extends Controller
 
 $collection = collect([
     ['product' => 'Desk', 'price' => 200],
-    ['product' => 'Chair', 'price' => 100],
+    ['product' => 'Chair', 'price' => 80],
     ['product' => 'Bookcase', 'price' => 150],
+    ['product' => 'Pencil', 'price' => 30],
     ['product' => 'Door', 'price' => 100],
 ]);
 
-$filtered = $collection->where('price', 100);
+$filtered = $collection->whereBetween('price', [100, 200]);
 
 $filtered->all();
 
