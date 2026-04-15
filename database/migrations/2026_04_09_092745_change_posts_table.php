301fb297ed86ad158ea389014d0b40af9f7260c9
Midina <milina_1979@list.ru>
2026-04-15 03:23:20 -0700

    Порядок поля (после поля)

--- database/migrations/2026_04_09_092745_change_posts_table.php
+++ database/migrations/2026_04_09_092745_change_posts_table.php
@@ -10,7 +10,7 @@ class ChangePostsTable extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-           $table->string('name')->first()->change();
+           $table->string('text')->after('vote')->change();
         });
     }
 }
