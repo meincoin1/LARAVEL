ad073a3ed6ff9334cccf9169432cde6371e60e38
Midina <milina_1979@list.ru>
2026-04-15 03:18:40 -0700

    Безнаковость

--- database/migrations/2026_04_09_092745_change_posts_table.php
+++ database/migrations/2026_04_09_092745_change_posts_table.php
@@ -10,8 +10,7 @@ class ChangePostsTable extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->string('desc')->comment('my comment');
+           $table->integer('votes')->unsigned();
         });
     }
 }
-;
