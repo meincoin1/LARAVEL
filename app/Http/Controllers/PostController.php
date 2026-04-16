f9d44460b2b750c706b333324816581796875215
Midina <milina_1979@list.ru>
2026-04-16 00:22:16 -0700

    Проверка

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,28 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-	
+
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Str;
-class PostController extends Controller 
+
+class PostController extends Controller
 {
     public function show()
     {
-        Collection::macro('toUpper', function () {
-            return $this->map(function ($value) {
-                return Str::upper($value);
-            });
-        });
-
-$collection = collect(['Chair', 'Desk']);
-
-$zipped = $collection->zip([100, 200]);
-
-$zipped->all();
-
-dd($zipped);
+        $posts = DB::table('posts')->get();
+        dump($posts);
     }
 }
-
-?>
