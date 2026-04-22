ec7b60dbacf7b7333efac5b9c0bfa5ee4b7ad795
Midina <milina_1979@list.ru>
2026-04-22 04:15:26 -0700

    Задача 7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ class UserController extends Controller
 {
     public function show()
     {
-        $users = DB::table('users')->select('name', 'email')->get();
+        $users = DB::table('users')->select('name', 'email as user_email')->get();
         dump($users);
     }
 }
