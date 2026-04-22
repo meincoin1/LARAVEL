4e938d1ce5c62dc106815d746d2dc702b2216254
Midina <milina_1979@list.ru>
2026-04-22 04:13:38 -0700

    Задача 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,17 +7,10 @@ class UserController extends Controller
 {
     public function show()
     {
-         // Способ 1: toSql()
-        $query = DB::table('posts')->where('id', '!=', 3)->toSql();
-        dump($query);
-        
-        // Способ 2: dd()
-        // DB::table('posts')->where('id', '!=', 3)->dd();
-        
-        // Способ 3: getQueryLog()
-        // DB::enableQueryLog();
-        // DB::table('posts')->where('id', '!=', 3)->get();
-        // dump(DB::getQueryLog());
+         $start = microtime(true);
+        DB::table('posts')->where('id', '!=', 3)->get();
+        $time = microtime(true) - $start;
+        echo "Время выполнения: {$time} секунд";
     }
 }
 
