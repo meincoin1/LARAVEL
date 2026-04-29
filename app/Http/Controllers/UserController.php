51b5dbc1438c70fb7898927df8e324c5d2e96a68
Midina <milina_1979@list.ru>
2026-04-28 19:59:45 -0700

    Задача 46

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ class UserController extends Controller
 {
     public function show()
     {
-        $users = DB::table('users')->skip(4)->take(10)->get();
+        $users = DB::table('users')->where('age', 30)->skip(2)->take(10)->get();
         dump($users);
     }
 }
