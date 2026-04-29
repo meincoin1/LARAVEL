a2e0d84ac4e6fb1072824c157e92a88b8b766cbe
Midina <milina_1979@list.ru>
2026-04-28 19:25:09 -0700

    Задача 18

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,8 +7,8 @@ class UserController extends Controller
 {
     public function show()
     {
-        $users = DB::table('users')->whereBetween('age', [20, 30])->orWhereBetween('salary', [400, 800])->get();
-        dump($users);
+        $user = DB::table('users')->where('id', 3)->first();
+        dump($user);
     }
 }
 
