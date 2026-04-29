79cf7079aff65a6a6d3f89f61ca2702ae85f93ad
Midina <milina_1979@list.ru>
2026-04-28 19:57:05 -0700

    Задача 39

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ class UserController extends Controller
 {
     public function show()
     {
-        $users = DB::table('users')->orderBy('updated_at', 'desc')->get();
+        $users = DB::table('users')->where('age', '>', 30)->orderBy('updated_at', 'asc')->get();
         dump($users);
     }
 }
