9c1c3e41a4c62eed3ea52db893f700f06ca2627d
Midina <milina_1979@list.ru>
2026-04-28 19:27:38 -0700

    Задача 19

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,10 @@ class UserController extends Controller
     public function show()
     {
         $user = DB::table('users')->where('id', 3)->first();
-        dump($user);
+        
+        echo "<p>Имя: {$user->name}</p>";
+        echo "<p>Возраст: {$user->age}</p>";
+        echo "<p>Email: {$user->email}</p>";
     }
 }
 
