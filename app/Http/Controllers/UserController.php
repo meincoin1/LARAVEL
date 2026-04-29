9938a120371daa0cd0d1f50d270861b9bbe050af
Midina <milina_1979@list.ru>
2026-04-28 19:40:19 -0700

    Задача 25

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ class UserController extends Controller
 {
     public function show()
     {
-        $users = DB::table('users')->whereBetween('age', [30, 40])->get();
+        $users = DB::table('users')->whereNotBetween('age', [30, 40])->get();
         dump($users);
     }
 }
