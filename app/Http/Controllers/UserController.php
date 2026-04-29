6b7e46fc3ee2a08461618180c9e99493a6c8dce5
Midina <milina_1979@list.ru>
2026-04-28 20:00:12 -0700

    Задача 46

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,8 +7,15 @@ class UserController extends Controller
 {
     public function show()
     {
-        $users = DB::table('users')->where('age', 30)->skip(2)->take(10)->get();
-        dump($users);
+        DB::table('users')->insert([
+            'name' => 'Новый юзер',
+            'email' => 'new@mail.com',
+            'age' => 25,
+            'salary' => 1000,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        echo "Юзер добавлен";
     }
 }
 
