d888a06cf2c86a0da06a7c64834f7537da2a5cbc
Midina <milina_1979@list.ru>
2026-05-05 23:26:40 -0700

    Задача 25.3

--- resources/views/posts/all.blade.php
+++ resources/views/posts/all.blade.php
@@ -2,12 +2,49 @@
 <html>
 <head>
     <title>Все статьи</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            border: 1px solid #ddd;
+            padding: 12px;
+            text-align: left;
+        }
+        th {
+            background-color: #1a3e6d;
+            color: white;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        h1 {
+            color: #333;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    @foreach ($posts as $post)
-        <div>
-            <h2>{{ $post->title }}</h2>
-        </div>
-    @endforeach
+    <h1>Список статей</h1>
+    
+    <table>
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>Заголовок</th>
+                <th>Описание</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+            <tr>
+                <td>{{ $post->id }}</td>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->desc }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 </html>
