32e293500d0417e7b78b09faa8dc7af455012a71
Midina <milina_1979@list.ru>
2026-05-12 22:21:59 -0700

    Задача 27.1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::match(['get', 'post'], '/post/update-or-create', [PostController::class, 
 
 // Задача 27.1 - удаление статьи
 Route::get('/post/del/{id}', [PostController::class, 'delPost']);
+
